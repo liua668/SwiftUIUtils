e0d91a2d9875ad48d5e95b7788e12792efb801db
ml <ml@gmail.com>
2025-06-16 18:08:08 +0800

    1.2.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "SwiftUIUtils",
             url: "https://github.com/liua668/SwiftUIUtils/releases/download/1.2.0/SwiftUIUtils.xcframework.zip",
-            checksum: "d7304f87fab5c607f3869337f1a2c1901f72f650027c7dc0a8e7c1562d2e07bb"
+            checksum: "8fa9c22897d08d767567d76740eca63f5db76574ee03b2c3c52a8d7c9e233f10"
         )
     ]
 )
